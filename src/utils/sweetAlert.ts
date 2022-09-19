be9c37a0a438de4a91cd2b7dc62f2b839b39cb20
pthangdev <pthangdev@gmail.com>
2022-09-19 23:56:23 +0700

    feat: add sweetAlert function

---
+++ src/utils/sweetAlert.ts
@@ -0,0 +1,56 @@
+import Swal from 'sweetalert2';
+
+interface Content {
+  title?: string;
+  text: string;
+}
+
+const sweetAlert = {
+  success(title: string = 'Successfully', duration: number = 3500, footer: string = 'Done!') {
+    return Swal.fire({
+      position: 'center',
+      icon: 'success',
+      title,
+      showConfirmButton: false,
+      timer: duration,
+      footer,
+    });
+  },
+
+  error(message: string = 'Server not responding!') {
+    return Swal.fire({
+      iconHtml: '<p class="swal2-icon-sad"></p>',
+      title: 'Error...',
+      text: message,
+      // footer: '<a href="">Why do I have this issue?</a>',
+    });
+  },
+  loading() {
+    return Swal.fire({
+      title: 'Loading...',
+      footer: 'Wait a minute!',
+      didOpen: () => {
+        Swal.showLoading();
+      },
+      allowEscapeKey: false,
+      allowOutsideClick: false,
+    });
+  },
+  confirm(content?: Content) {
+    return Swal.fire({
+      title: content?.title || 'Are you sure?',
+      text: content?.text || "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes',
+    });
+  },
+
+  close() {
+    return Swal.close();
+  },
+};
+
+export default sweetAlert;
